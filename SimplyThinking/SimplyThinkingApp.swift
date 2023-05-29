a63a4282efcc020598fbb9e85fbe1e640b0c95c9
kathlynee <ksarahfania@student.ciputra.ac.id>
2023-05-30 01:06:42 +0700

    Initial Commit

---
+++ SimplyThinking/SimplyThinkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplyThinkingApp.swift
+//  SimplyThinking
+//
+//  Created by MacBook Pro on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SimplyThinkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
